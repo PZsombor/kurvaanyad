1d7ef47d7918412eef337557775e519df026805f
Paulusz Zsombor <paulusz.zsombor@gmail.com>
2024-02-07 11:09:15 +0100

    xdxd

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [piece, setPiece] = useState();
+  function calcValue() {
+    alert(":) " + piece)
+  }
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Gyalog</Text>
+      </View>
+      
+      <View style={styles.body}>
+        <Text>Áldozatok</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={piece => setPiece(piece)}/>
+        <Button
+          title='Értékelés'
+          onPress={calcValue}
+        />
+        <Text>{}</Text>
+        
+      </View>
+      
     </View>
   );
 }
@@ -16,5 +37,37 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  header: {
+    backgroundColor: 'cyan',
+    height: 50,
+    width: '100%',
+
+  },
+
+  headerText: {
+    textAlign: 'center',
+    fontSize: 20,
+    fontFamily: 'bold'
+  },
+
+  body: {
+    flex: 1,
+    backgroundColor: 'skyblue',
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  input: {
+    backgroundColor: 'white',
+    width: '80%',
+    marginBottom: 10,
+    marginTop: 10,
+    height: 40,
+    fontSize: 25,
+    borderRadius: 10,
+    padding: '10px'
   },
 });
